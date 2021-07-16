4ce31bf5ab5446a5582a04071d15002f3a0e65e1
chuksoo <mexx4me@yahoo.com>
2021-07-16 04:53:57 -0800

    Update nb_author_id.py

--- Machine Learning Engineer Nanodegree/Mini Projects/Intro to Machine Learning/naive_bayes/nb_author_id.py
+++ Machine Learning Engineer Nanodegree/Mini Projects/Intro to Machine Learning/naive_bayes/nb_author_id.py
@@ -23,20 +23,6 @@ features_train, features_test, labels_train, labels_test = preprocess()
 clf = GaussianNB()
 t0 = time()
 clf.fit(features_train, labels_train)
-print('Training time:', round(time()-t0, 3), 's')
-
-
-
-
-#########################################################
-### your code goes here ###
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-t0=time()
-clf.fit(features_train, labels_train)
 print('Training time: ', round(time() - t0, 3), 's')
-
-
-#########################################################
 
 
