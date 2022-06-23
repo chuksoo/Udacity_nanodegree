d3a66436e74dc5366fbaf41e3db4943bd114b72e
chuksoo <mexx4me@yahoo.com>
2022-06-22 16:48:23 -0800

    AI programming with Python

---
+++ AI Programming with Python Nanodegree/Data Types and Operators - Arithmetic Operators - Quiz 1.py
@@ -0,0 +1,13 @@
+# Quiz: Average Electricity Bill
+# My electricity bills for the last three months have been $23, $32 and $64. What is the average monthly electricity bill over the three month period? 
+# Write an expression to calculate the mean, and use print() to view the result.
+print((23 + 32 + 64)/3)
+
+
+# In this quiz you're going to do some calculations for a tiler. Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other 
+# is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+# How many tiles are needed?
+# You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+print(9*7 + 5*7)
+# Fill this in with an expression that calculates how many tiles will be left over.
+print(17*6 - (9*7 + 5*7))
